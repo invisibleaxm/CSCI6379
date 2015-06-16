8db6679b148ddb09ce38b2188001114e6f7184d7
Alex Campos <alex@invisibleaxm.com>
2015-06-16 17:34:35 -0500

    Created variable for max_links that way we limit the number of fetched documents

--- webcrawler.py
+++ webcrawler.py
@@ -40,21 +40,42 @@ import re
 
 #start_url = 'http://portal.utpa.edu/utpa_main/daa_home/coecs_home/cs_home'
 #start_url = 'http://portal.utpa.edu/utpa_main/daa_home/coecs_home/cs_home'
-start_url = 'http://www.dmoz.org/Computers/Programming/Languages/Python/Resources'
-base_os_dir = os.getcwd()+"/document_corpus"
-
 #start_url = 'https://my.utpa.edu/home'
 
+#start webpage of our crawler
+start_url = 'http://www.dmoz.org/Computers/Programming/Languages/Python/Resources'
+
+#where to store the files a.k.a. document corpus.
+base_os_dir = os.getcwd()+"/document_corpus"
+
+
+# this list data structure will keep track of all "good" urls. By good we mean
+# documents that we were able to parse and save to disk
 links = []
+
+#this keeps track of the documents that returned page not found,
+# documents we had no access or couldnt write to disk. This way we dont waste
+# time trying to access them again
 bad_links = []
+
+#since we want to limit our crawler to a "small" document corpus, we define a
+# variable that will limit the number of files fetched.
+max_links = 4
+
+create_subfolders = False
+
+
 
 # # # # # # # # # # # # # # # # #
 # Simple function that will help "normalize" links. In other words, this function wil prepend the
 # base_url if needed.
 # # # # # # # # # # # # # # # # # #
-def get_url(link):
+def get_url(link, base_url):
+    parsed_url = urlparse(base_url)
     if(link[0]=='/'):
-        return start_url+link
+        good_url = parsed_url.scheme + "://" + parsed_url.netloc +link
+        #print("returning url: " + good_url)
+        return good_url
     else:
         return link
 
@@ -73,36 +94,39 @@ def is_domain_allowed(link):
 #        print("Not allowed: "+link)
         return False
 
-def follow_links(html_page):
+def follow_links(html_page, base_url):
     bs = BeautifulSoup(html_page)
     try:
         for link in bs.find_all("a", href=re.compile('^http://|^/')) :
-            if link.has_attr('href') and is_domain_allowed(link['href']) and len(links) < 100:
-                my_url = get_url(link["href"])
-                if my_url not in links and len(links) < 100:
+            if link.has_attr('href') and is_domain_allowed(link['href']) and len(links) < max_links:
+                my_url = get_url(link["href"], base_url)
+                if my_url not in links and my_url not in bad_links:
                     get_html(my_url)
+
     except:
-        pass
+        traceback.print_exc(file=sys.stdout)
 
 
 def get_html(url):
     try:
-        my_url = get_url(url)
+        my_url = get_url(url, url)
         if my_url not in links and my_url not in bad_links:
             print("Following URL: ", url)
        	    my_headers = { 'User-Agent': 'Mozilla/5.0' }
             r = requests.get(url, headers = my_headers)
             if r.status_code == requests.codes.ok:
                 page = r.text
-                folder_structure = urlparse(url)
-                #os_folderstructure = base_os_dir + folder_structure.path.rsplit('/',1)[0] ## add folder names from url
                 filename = url.split('/')[-1].split('#')[0].split('?')[0]
                 if "." not in filename:
                     filename = filename + ".html"
-       	        #if not os.path.exists(os_folderstructure):
-                #    os.makedirs(os_folderstructure,exist_ok=True)
-                #full_filename = os.path.join(os_folderstructure + "/" + filename)
-                full_filename = base_os_dir + "/" + filename
+                if create_subfolders == True: #does the user prefer to create folder structure based on url parsing?
+                    folder_structure = urlparse(url)
+                    os_folderstructure = base_os_dir + folder_structure.path.rsplit('/',1)[0] ## add folder names from url
+       	            if not os.path.exists(os_folderstructure):
+                        os.makedirs(os_folderstructure,exist_ok=True)
+                        full_filename = os.path.join(os_folderstructure + "/" + filename)
+                else:
+                    full_filename = base_os_dir + "/" + filename
                 #print(os_folderstructure)
                 print("Using Filename: " , filename)
                 #print(page)
@@ -110,13 +134,14 @@ def get_html(url):
                     try:
                         with open(full_filename, 'w') as f:
                             f.write(page)
+                            links.append(my_url)
+                            print("Successfully fetched url {} now we are about to follow it's links".format(my_url))
+                            if len(links) < max_links: ## make this a parameter
+                                follow_links(page, my_url)
                     except:
-                        print("There was an error writing to file, flagging this page as bad")
+                        print("There was an error writing to file, flagging this page {} as bad".format(my_url))
                         bad_links.append(my_url)
-                links.append(my_url)
-                print("Successfully fetched url {} now we are about to follow it's links".format(my_url))
-                if len(links) < 100:
-                    follow_links(page)
+                        os.remove(full_filename) #do some cleanup
             else:
                 bad_links.append(my_url)
                 print("Page couldnt be fetched. Response with status code {} ".format(r.status_code))
@@ -128,8 +153,10 @@ def get_html(url):
         #traceback.print_exc(file=sys.stdout)
         #pass
 
-def __main__:
+if __name__ == '__main__':
     print("Welcome to CSCI 6370 web crawler")
     get_html(start_url)
     print("Total number of documents retrieved: {} ".format(len(links)))
+    #print(links)
     print("Total number of bad documents: {}".format(len(bad_links)))
+    #print(bad_links)
