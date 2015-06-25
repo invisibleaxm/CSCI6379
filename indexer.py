d15e13eada9592677c8204ea09c3572b51ccaadd
Alex Campos <alex@invisibleaxm.com>
2015-06-25 13:59:59 -0500

    Added docinfo class and extracting document words

--- indexer.py
+++ indexer.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import csv
+from docinfo import docinfo
 #from nltk.stem.lancaster import LancasterStemmer
 
 
@@ -17,6 +18,8 @@ invindex = defaultdict(dict)
 
 stop = stopwords.words('english')
 st = PorterStemmer
+
+document_table = {} #index of documents and their information
 
 
 #f = open('document_corpus/1.htm', 'r')
@@ -31,11 +34,15 @@ def remove_puctuation(s):
                         ord('\''): ''  })
 
 
-def index_document(id, filename, url):
+
+def index_document(id, filename):
     soup = BeautifulSoup(open(filename))
     [x.extract() for x in soup.findAll('script')]
-    raw_text = soup.getText()
-    text = remove_puctuation(raw_text)
+    text = remove_puctuation(soup.getText())
+    title = soup.title.contents[0]
+    snip = text.split()[60:80]
+    document_table[mydoc.id].title = title
+    document_table[mydoc.id].snip = " ".join(snip)
     #text = soup.getText()
     terms = [i.lower() for i in text.split() if i not in stop]
     for term in terms:
@@ -63,10 +70,16 @@ if __name__ == '__main__':
     with open('crawled_files.txt', 'r') as inputfile:
         reader = csv.DictReader(inputfile)
         for row in reader:
-            index_document(row['index'], row['filename'], row['url'])
-            #print(row['index'], row['filename'], row['url'])
-    print("Searching for keyword: research")
-    print(invindex["research"])
+            mydoc = docinfo()
+            mydoc.id = row['index']
+            mydoc.filename = row['filename']
+            mydoc.url = row['url']
+            document_table[mydoc.id] = mydoc
+            index_document(mydoc.id, mydoc.filename)
+        #    #print(row['index'], row['filename'], row['url'])
+    #print("Searching for keyword: research")
+    #print(invindex["research"])
+    #print(document_table['2'].filename)
 
     #for i in range(1,100):
      #   index_document(str(i) + '.htm')
@@ -75,9 +88,9 @@ if __name__ == '__main__':
 
 
 
-#for key, value in invindex.items():
+#for key, value in document_table.items():
 #    try:
-#        print(key,value)
+#        print(value.id, value.snip)
 #    except:
 #        pass
 
