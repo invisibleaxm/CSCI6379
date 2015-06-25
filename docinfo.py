f49f60599b0dcfab965db6186f436e51e051d840
Alex Campos <alex@invisibleaxm.com>
2015-06-25 14:02:50 -0500

    Added actual class to commit.

---
+++ docinfo.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+class docinfo:
+
+    def __init__(self):
+        self.id = ''
+        self.filename = ''
+        self.url = ''
+        self.title = ''
+        self.snip = ''
+
